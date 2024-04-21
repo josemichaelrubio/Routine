034957b5c4face922a89ec9547d6fecc46fa0bac
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-04-21 10:48:10 -0400

    It works, but need to fix closing the apps on mac

--- morning.py
+++ morning.py
@@ -9,16 +9,24 @@ load_dotenv()
 # Function to open a Word document
 def open_app(path):
     if sys.platform.startswith('darwin'):
-        subprocess.call(['open', path])
+        proc = subprocess.Popen(['open', '-W', path])
+        proc.wait()
     else:
-        subprocess.Popen(['start', path], shell=True)
+        proc = subprocess.Popen(['start', path], shell=True)
+        proc.wait()
 
 
 word_path = os.getenv('DEFINITE_STATEMENT_WORD_PATH')
-onenote_path = os.getenv('WINDOWS_ONENOTE_PATH')
+# NOT CONTINUING SEQUENCE:
+#! onenote_path = os.getenv('WINDOWS_ONENOTE_PATH')
+onenote_path = os.getenv('MAC_ONENOTE_PATH')
+todoist_path = os.getenv('MAC_TODOIST_PATH')
+outlook_path = os.getenv('MAC_OUTLOOK_PATH')
 
 # Running the sequence of actions
 open_app(word_path)
 open_app(onenote_path)
+open_app(todoist_path)
+open_app(outlook_path)
 
 
